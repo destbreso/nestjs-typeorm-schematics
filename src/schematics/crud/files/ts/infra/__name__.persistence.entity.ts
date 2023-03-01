abb37b25daf3b71a5bc8d696810589eb2011d455
destbreso <destevezbreso@gmail.com>
2023-03-01 13:52:29 -0500

    minor fix

--- src/schematics/crud/files/ts/infra/__name__.persistence.entity.ts
+++ src/schematics/crud/files/ts/infra/__name__.persistence.entity.ts
@@ -2,7 +2,7 @@ import { Entity } from 'typeorm'
 import { MinimalEntity } from '@nestjs-data-access/nestjs-data-access'
 
 @Entity('<%= lowerCase(name) %>')
-export class <%= classify(name) %> Persistence extends MinimalEntity {
+export class <%= classify(name) %>Persistence extends MinimalEntity {
 
     //custom props typeorm persistence
 }
