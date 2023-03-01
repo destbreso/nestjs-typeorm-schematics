dcf6d619396665d338efaa54f9b1d67ac647bf0f
destbreso <destevezbreso@gmail.com>
2023-03-01 14:39:35 -0500

    update reference

--- src/schematics/crud/files/ts/infra/__name__.persistence.entity.ts
+++ src/schematics/crud/files/ts/infra/__name__.persistence.entity.ts
@@ -1,5 +1,5 @@
 import { Entity } from 'typeorm'
-import { MinimalEntity } from '@nestjs-data-access/nestjs-data-access'
+import { MinimalEntity } from '@guachos/nestjs-typeorm-data-access'
 
 @Entity('<%= lowerCase(name) %>')
 export class <%= classify(name) %>Persistence extends MinimalEntity {
