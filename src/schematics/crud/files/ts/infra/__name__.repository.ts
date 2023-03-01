3f099660eacf155d730b550cb1114d01404f92df
destbreso <destevezbreso@gmail.com>
2023-03-01 16:25:33 -0500

    minor change

--- src/schematics/crud/files/ts/infra/__name__.repository.ts
+++ src/schematics/crud/files/ts/infra/__name__.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { BuildCRUDRepository } from "@nestjs-typeorm-recipes/nestjs-typeorm-recipes";
+import { BuildCRUDRepository } from "@guachos/nestjs-typeorm-recipes";
 import { <%= classify(name) %>Domain } from "../domain/<%= lowerCase(name) %>.domain.entity";
 import { <%= classify(name) %>Persistence } from "./<%= lowerCase(name) %>.persistence.entity";
 import { <%= classify(name) %>Mapper } from "./<%= lowerCase(name) %>.mapper";
